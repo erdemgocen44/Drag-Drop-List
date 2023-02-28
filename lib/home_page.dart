b500cd8bc7271a89ea9dbdca9d8e1723ecff301a
Erdem <erdemgocen44@gmail.com>
2023-02-28 21:12:00 +0100

    drag drop

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List myTiles = [
+    'Aliquam tincidunt mauris eu risus.',
+    'Vestibulum auctor dapibus neque.',
+    'Nunc dignissim risus id metus.',
+    'Cras ornare tristique elit.',
+    'Vivamus vestibulum ntulla nec ante.'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +24,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Drag et Drop List'),
       ),
+      body: HomePage(),
     );
   }
 }
